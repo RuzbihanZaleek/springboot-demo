7b19dec43c0fea9748f41effd3bf1189496d2251
Ruzbihan <ruzbihanmzm@gmail.com>
2024-04-19 18:23:18 +0530

    initial commit

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class UserController {
+
+    @GetMapping("/getUser")
+    public String getUser() {
+        return "Spring Boot";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser() {
+        return "User Saved!";
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser() {
+        return "User Updated!";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser() {
+        return "User Deleted!";
+    }
+}
