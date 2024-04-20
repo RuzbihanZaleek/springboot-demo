8546656a4c8df968b8c7948efb2a07f189cd84cd
Ruzbihan <ruzbihanmzm@gmail.com>
2024-04-20 13:59:28 +0530

    @GetMapping

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
@@ -13,8 +15,8 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/getUser")
-    public String getUser() {
-        return "Spring Boot";
+    public List<UserDTO> getUser() {
+        return userService.getAllUsers();
     }
 
     @PostMapping("/saveUser")
