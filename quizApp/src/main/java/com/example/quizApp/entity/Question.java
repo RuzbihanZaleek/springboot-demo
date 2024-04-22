6e693856a86576193c7e4561f3f286cb8cc749ad
Ruzbihan <ruzbihanmzm@gmail.com>
2024-04-22 23:58:33 +0530

    question entity

---
+++ quizApp/src/main/java/com/example/quizApp/entity/Question.java
@@ -0,0 +1,25 @@
+package com.example.quizApp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String questionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String rightAnswer;
+    private String difficultyLevel;
+    private String category;
+}
