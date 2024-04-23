60dceef619a91e1180318a8d450a63064c7085ad
Ruzbihan <ruzbihanmzm@gmail.com>
2024-04-23 18:26:42 +0530

    Fetch Quiz

--- quizApp/src/main/java/com/example/quizApp/controller/QuizController.java
+++ quizApp/src/main/java/com/example/quizApp/controller/QuizController.java
@@ -1,10 +1,13 @@
 package com.example.quizApp.controller;
 
+import com.example.quizApp.entity.QuestionWrapper;
 import com.example.quizApp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -13,8 +16,13 @@ public class QuizController {
     @Autowired
     QuizService quizService;
 
-    @PostMapping("createQuiz")
+    @PostMapping(value = "createQuiz")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping(value = "getQuiz/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable int id) {
+        return quizService.getQuizQuestions(id);
     }
 }
--- quizApp/src/main/java/com/example/quizApp/service/QuizService.java
+++ quizApp/src/main/java/com/example/quizApp/service/QuizService.java
@@ -2,6 +2,7 @@ package com.example.quizApp.service;
 
 import com.example.quizApp.dto.QuestionDTO;
 import com.example.quizApp.entity.Question;
+import com.example.quizApp.entity.QuestionWrapper;
 import com.example.quizApp.entity.Quiz;
 import com.example.quizApp.repo.QuestionRepo;
 import com.example.quizApp.repo.QuizRepo;
@@ -12,7 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -34,5 +37,19 @@ public class QuizService {
         quizRepo.save(quiz);
 
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(int id) {
+        Optional<Quiz> quiz = quizRepo.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for (Question q : questionsFromDB) {
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
